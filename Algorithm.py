a2fd3df3c8086766849b57dd2b8edeccf0f9432d
@yvulight <yvonhuben@yetnet.ch>
2024-05-03 08:18:38 +0200

    external start

--- Algorithm.py
+++ Algorithm.py
@@ -8,12 +8,25 @@ input = json.load(f)
 output = input.copy()
 
 #Tells which Elevator to do something
-if input['input']['is_internal'] == True:
-    print("it is internal")
+if input['input']['is_internal']:
+    lift_number = input['input']['internal']['lift']
+    target = input['input']['internal']['storey']
+    output['state']['lifts'][lift_number]['targets'].append(target)
 
-print(output)
+if not input['input']['is_internal']:
+    where_pressed = input['input']['external']['storey']
+    direction_upwards = input['input']['external']['upwards']
+    # jetzt kommt die heilige Frage, welcher Lift hohlt diese Person ab?
+     
+
+#targets ordnen
+
+
 
 
 
 
 f.close()
+
+with open('algo_output.json', 'w', encoding='utf-8') as v:
+    json.dump(output, v, ensure_ascii=False, indent=4)
