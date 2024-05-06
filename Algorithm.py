6fa6d3fa648ef5d5ff19056c4ec4f50c62a0b6cc
yvulight <155193362+yvulight@users.noreply.github.com>
2024-05-06 21:23:34 +0200

    write some comments

--- Algorithm.py
+++ Algorithm.py
@@ -1,4 +1,4 @@
-import json
+timport json
 
 # Opening JSON file
 f = open('Input.json')
@@ -7,16 +7,19 @@ f = open('Input.json')
 input = json.load(f)
 output = input.copy()
 
-#Tells which Elevator to do something
+#Falls der Input im Lift ist, muss einfach das stockwerk der Target liste hinzugefügt werden
 if input['input']['is_internal']:
     lift_number = input['input']['internal']['lift']
     target = input['input']['internal']['storey']
     output['state']['lifts'][lift_number]['targets'].append(target)
 
+#Falls der Input extern ist, muss heruasgefunden werden welcher Lift die Person abhohlt
 if not input['input']['is_internal']:
     where_pressed = input['input']['external']['storey']
     direction_upwards = input['input']['external']['upwards']
     # jetzt kommt die heilige Frage, welcher Lift hohlt diese Person ab?
+    #ev. check ob Lift hier ist; ob ein Lift vorbeifähret(der noch platz hat);welcher Lift denn sonst der nächste ist, falls keiner was macht
+    
      
 
 #targets ordnen
